27e3d2726b97bf16d2d10c2d6dbb58e69a3cf245
Joe Ingram <josephcingram@hotmail.co.uk>
2017-08-31 20:50:58 +0100

    Docking station now releases working bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,8 @@ describe DockingStation do
   it "releases bike using bike_release method" do
     expect(subject).to respond_to :release_bike
   end
+  it "releases working bikes" do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
