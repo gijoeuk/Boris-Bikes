3aa3a55661c52ae8a9c61d9c8cbe101f6c425035
Luan van Pletsen <luanvanpletsen@msn.com>
2017-09-01 10:17:22 +0100

    dock test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,13 +7,27 @@ describe DockingStation do
     it "releases bike using bike_release method" do
       expect(subject).to respond_to :release_bike
     end
+
     it "releases working bikes" do
       bike = subject.release_bike
       expect(bike).to be_working
     end
 
   end
+
   describe "#dock" do
+
+    it "docks a bike" do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+    end
+
+  end
+
+  describe "#bike" do
+
+
+
 
   end
 
