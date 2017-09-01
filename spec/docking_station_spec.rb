c925ebf736452abaa3398e79dc72300a1f8407d7
Luan van Pletsen <luanvanpletsen@msn.com>
2017-09-01 09:46:25 +0100

    refactor

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,21 @@
 require 'docking_station'
 
 describe DockingStation do
-  it "releases bike using bike_release method" do
-    expect(subject).to respond_to :release_bike
+
+  describe "#release_bike" do
+
+    it "releases bike using bike_release method" do
+      expect(subject).to respond_to :release_bike
+    end
+    it "releases working bikes" do
+      bike = subject.release_bike
+      expect(bike).to be_working
+    end
+
   end
-  it "releases working bikes" do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  describe "#dock" do
+
   end
+
+
 end
